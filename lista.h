bf6ae3a485adf59b71955f8bada073ec3550d120
Ensuo <63194069+Ensuo@users.noreply.github.com>
2022-10-04 16:21:33 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -7,10 +7,10 @@
     typedef struct lista_ list;
 
     list *lista_criar(void);
-    bool lista_inserir(list *lista, int chave);
-    Node *lista_remover(list *lista, int chave);
+    bool lista_inserir(list *lista, int dist, int id);
+    Node *lista_remover(list *lista, int id);
     bool lista_apagar(list **lista);
-    Node*lista_busca(list *lista, int chave);
+    Node*lista_busca(list *lista, int id);
     int lista_tamanho(list *lista);
     bool lista_vazia(list *lista);
     bool lista_cheia(list *lista);
