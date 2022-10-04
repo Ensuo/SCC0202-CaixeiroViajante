da93eb0dfa82eaa38f58346b87d9c4afc68a2c81
Ensuo <haradayasuo2010@gmail.com>
2022-10-04 16:53:19 -0300

    Comentei as funções que nao estamos usando no lista.h

--- lista.h
+++ lista.h
@@ -8,13 +8,13 @@
 
     list *lista_criar(void);
     bool lista_inserir(list *lista, int dist, int id);
-    Node *lista_remover(list *lista, int id);
+    //No* lista_remover(list *lista, int id);dsadasd
     bool lista_apagar(list **lista);
-    Node*lista_busca(list *lista, int id);
+    //No* lista_busca(list *lista, int id);
     int lista_tamanho(list *lista);
     bool lista_vazia(list *lista);
-    bool lista_cheia(list *lista);
-    void lista_imprimir(list *lista);
-    int distancia_nodes(list *lista, Node* a, Node* b);
+    // bool lista_cheia(list *lista);
+    // void lista_imprimir(list *lista);
+    // int distancia_nodes(list *lista, Node* a, Node* b);
 
 #endif
